d62b3f90c55151693a645416c13ddbb5e0d7c297
cristiancfe <cristian.eisenhut@pampa.compasso>
2021-06-24 19:17:03 -0300

    alteração javascript

--- js/main.js
+++ js/main.js
@@ -9,9 +9,9 @@ console.log(frase)
 console.log(frase.toLowerCase())
 console.log(frase.toUpperCase())*/
 //imprime a lista
-var lista = ['maçã', 'pêra', 'laranja']
-    //console.log(lista)
-    //comando PUSH adiciona elemento na lista
+/*var lista = ['maçã', 'pêra', 'laranja']
+//console.log(lista)
+//comando PUSH adiciona elemento na lista
 lista.push('uva')
 console.log(lista)
     // comando POP retira ultimo elemento da lista
@@ -24,3 +24,45 @@ console.log(lista.toString())
 console.log(lista.toString()[0])
     //Join tras os elementos separados por um traço -
 console.log(lista.join(' - '))
+    //dicionario semelhante a lista, mas com nome e cor(Json)
+*/
+/*var fruta = { nome: 'maçã', cor: 'vermelha' }
+console.log(fruta)
+console.log(fruta.nome)
+console.log(fruta.cor)
+    //pergunta através de variável com armazenamento da resposta
+var idade = prompt('qual sua idade')
+if (idade >= 18) {
+    alert('Maior de idade')
+} else {
+    alert('menor de idade')
+}*/
+/*var count = 0
+while (count <= 5) {
+    console.log(count)
+    alert(count)
+    count++
+}
+// for
+var count
+for (count = 0; count <= 5; count++) {
+    alert(count)
+}*/
+
+// data atual
+/*
+data = new Date()
+alert(data)*/
+// funções
+/*
+function soma(n1, n2) {
+    return n1 + n2
+}
+alert(soma(5, 10))
+console.log(soma(5, 10))*/
+/*
+function setReplace(frase, nome, novo_nome) {
+    return frase.replace(nome, novo_nome)
+}
+alert(setReplace('Vai Japão', 'Japão', 'Brasil'))
+*/

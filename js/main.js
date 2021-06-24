130c68d10cd7b765109a89c895cb1cae011aa6f7
cristiancfe <cristian.eisenhut@pampa.compasso>
2021-06-24 17:12:17 -0300

    arrays e formatação de arrays

---
+++ js/main.js
@@ -0,0 +1,26 @@
+/*var nome = 'Cristian Eisenhut'
+var idade = 48
+    //alert(nome + ' tem ' + idade + ' anos ')
+    //console.log(nome + ' tem ' + idade + ' anos ')
+var frase = ' japão é o melhor time do mundo'
+console.log(frase)
+// troca a palavra japão por Brasil
+//console.log(frase.replace('japão', 'Brasil'))
+console.log(frase.toLowerCase())
+console.log(frase.toUpperCase())*/
+//imprime a lista
+var lista = ['maçã', 'pêra', 'laranja']
+    //console.log(lista)
+    //comando PUSH adiciona elemento na lista
+lista.push('uva')
+console.log(lista)
+    // comando POP retira ultimo elemento da lista
+lista.pop(2)
+console.log(lista.length)
+console.log(lista.reverse())
+    //ToString tras em estado de string e não lista
+console.log(lista.toString())
+    //tras a primeira letra
+console.log(lista.toString()[0])
+    //Join tras os elementos separados por um traço -
+console.log(lista.join(' - '))
